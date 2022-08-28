42709fe06b4c54f5509d2cb672ee68da71528686
dxlearn <dx_learn@163.com>
2022-08-28 22:03:11 +0800

    update http

--- http/http_conn.cpp
+++ http/http_conn.cpp
@@ -402,6 +402,258 @@ http_conn::HTTP_CODE http_conn::process_read()
 }
 
 
+http_conn::HTTP_CODE http_conn::do_request()
+{
+    strcpy(m_real_file, doc_root);
+    int len = strlen(doc_root);
+    //printf("m_url:%s\n", m_url);
+    const char *p = strrchr(m_url, '/');
+
+    //处理cgi
+    if (cgi == 1 && (*(p + 1) == '2' || *(p + 1) == '3'))
+    {
+
+        //根据标志判断是登录检测还是注册检测
+        char flag = m_url[1];
+
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/");
+        strcat(m_url_real, m_url + 2);
+        strncpy(m_real_file + len, m_url_real, FILENAME_LEN - len - 1);
+        free(m_url_real);
+
+        //将用户名和密码提取出来
+        //user=123&passwd=123
+        char name[100], password[100];
+        int i;
+        for (i = 5; m_string[i] != '&'; ++i)
+            name[i - 5] = m_string[i];
+        name[i - 5] = '\0';
+
+        int j = 0;
+        for (i = i + 10; m_string[i] != '\0'; ++i, ++j)
+            password[j] = m_string[i];
+        password[j] = '\0';
+
+        //同步线程登录校验
+        if (*(p + 1) == '3')
+        {
+            //如果是注册，先检测数据库中是否有重名的
+            //没有重名的，进行增加数据
+            char *sql_insert = (char *)malloc(sizeof(char) * 200);
+            strcpy(sql_insert, "INSERT INTO user(username, passwd) VALUES(");
+            strcat(sql_insert, "'");
+            strcat(sql_insert, name);
+            strcat(sql_insert, "', '");
+            strcat(sql_insert, password);
+            strcat(sql_insert, "')");
+
+            if (users.find(name) == users.end())
+            {
+
+                m_lock.lock();
+                int res = mysql_query(mysql, sql_insert);
+                users.insert(pair<string, string>(name, password));
+                m_lock.unlock();
+
+                if (!res)
+                    strcpy(m_url, "/log.html");
+                else
+                    strcpy(m_url, "/registerError.html");
+            }
+            else
+                strcpy(m_url, "/registerError.html");
+        }
+        //如果是登录，直接判断
+        //若浏览器端输入的用户名和密码在表中可以查找到，返回1，否则返回0
+        else if (*(p + 1) == '2')
+        {
+            if (users.find(name) != users.end() && users[name] == password)
+                strcpy(m_url, "/welcome.html");
+            else
+                strcpy(m_url, "/logError.html");
+        }
+    }
+
+    if (*(p + 1) == '0')
+    {
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/register.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+        free(m_url_real);
+    }
+    else if (*(p + 1) == '1')
+    {
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/log.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+        free(m_url_real);
+    }
+    else if (*(p + 1) == '5')
+    {
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/picture.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+        free(m_url_real);
+    }
+    else if (*(p + 1) == '6')
+    {
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/video.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+        free(m_url_real);
+    }
+    else if (*(p + 1) == '7')
+    {
+        char *m_url_real = (char *)malloc(sizeof(char) * 200);
+        strcpy(m_url_real, "/fans.html");
+        strncpy(m_real_file + len, m_url_real, strlen(m_url_real));
+
+        free(m_url_real);
+    }
+    else
+        strncpy(m_real_file + len, m_url, FILENAME_LEN - len - 1);
+
+    if (stat(m_real_file, &m_file_stat) < 0)
+        return NO_RESOURCE;
+    if (!(m_file_stat.st_mode & S_IROTH))
+        return FORBIDDEN_REQUEST;
+    if (S_ISDIR(m_file_stat.st_mode))
+        return BAD_REQUEST;
+    int fd = open(m_real_file, O_RDONLY);
+    m_file_address = (char *)mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+    return FILE_REQUEST;
+}
+
+
+void http_conn::unmap()
+{
+    if (m_file_address)
+    {
+        munmap(m_file_address, m_file_stat.st_size);
+        m_file_address = 0;
+    }
+}
+
+
+bool http_conn::write()
+{
+    int temp = 0;
+    if(bytes_to_send == 0)
+    {
+        modfd(m_epollfd,m_sockfd,EPOLLIN);
+        init();
+        return true;
+    }
+
+    while(1)
+    {
+        temp = writev(m_sockfd,m_iv,m_iv_count);
+        if(temp < 0)
+        {
+            if(errno == EAGAIN)
+            {
+                modfd(m_epollfd,m_sockfd,EPOLLOUT);
+            }
+            unmap();
+            return false;
+        }
+        bytes_have_send += temp;
+        bytes_to_send -= temp;
+        if(bytes_have_send >= m_iv[0].iov_len)
+        {
+            m_iv[0].iov_len = 0;
+            m_iv[1].iov_base = m_file_address + (bytes_to_send - m_write_idx);
+            m_iv[1].iov_len = bytes_to_send;
+        }
+        else
+        {
+            m_iv[0].iov_base = m_write_buf + bytes_to_send;
+            m_iv[0].iov_len = m_iv[0].iov_len - bytes_have_send;
+        }
+
+        if(bytes_to_send <= 0)
+        {
+            unmap();
+            modfd(m_sockfd,m_sockfd,EPOLLIN);
+
+            if(m_linger)
+            {
+                init();
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
+
+
+
+bool http_conn::add_response(const char *format,...)
+{
+    if(m_write_idx >= WRITE_BUFFER_SIZE)
+        return false;
+    //关于可变参：https://jingyan.baidu.com/article/adc815133f22a2b622bf7308.html
+    va_list arg_list;;//实际上是一个char*类型指针类型，在函数中用va_list定义一个指针变量，该指针变量用来指向函数的可变参数变量
+    va_start(arg_list,format);//va_start宏初始化arg_list变量，使其指向第一个可变参变量的地址
+    //int vsnprintf (char * s, size_t n, const char * format, va_list arg );
+    //https://blog.csdn.net/swif_N_F/article/details/78712315
+    int len = vsnprintf(m_write_buf + m_write_idx,WRITE_BUFFER_SIZE-1-m_write_idx,format,arg_list);
+    if(len >= (WRITE_BUFFER_SIZE -1 - m_write_idx))
+    {
+        va_end(arg_list);
+        return false;
+    }
+    va_end(arg_list);    //使用va_end宏清空va_list变量，使其重新赋值为0，结束对可变参数变量值的获取
+    // LOG_INFO("request:%s", m_write_buf);
+    // Log::get_instance()->flush();
+    return true;
+}
+
+bool http_conn::add_status_line(int status,const char *title)
+{
+    return add_response("%s %d %s\r\n",status,title);
+}
+
+bool http_conn::add_headers(int content_len)
+{
+    add_content_length(content_len);
+    add_linger();
+    add_blank_line();
+}
+
+bool http_conn::add_content_length(int content_len)
+{
+    return add_response("Content-Length:%d\r\n",content_len);
+}
+
+bool http_conn::add_content_type()
+{
+    return add_response("Content-Type:%s\r\n","text/html");
+}
+
+bool http_conn::add_linger()
+{
+    return add_response("Connection:%s\r\n",(m_linger == true) ? "keep-alive":"close");
+}
+
+bool http_conn::add_blank_line()
+{
+    return add_response("%s","\r\n");
+}
+
+bool http_conn::add_content(const char *content)
+{
+    return add_response("%s",content);
+}
+
 bool http_conn::process_write(HTTP_CODE ret)
 {
     switch(ret)
@@ -481,16 +733,9 @@ void http_conn::process()
 }
 
 
-
-
-
-
-
-
-
-void reset_oneshot(int epollfd,int fd){//重置事件
-    epoll_event event;
-    event.data.fd=fd;
-    event.events=EPOLLIN|EPOLLET|EPOLLONESHOT;
-    epoll_ctl(epollfd,EPOLL_CTL_MOD,fd,&event);
-}
+// void reset_oneshot(int epollfd,int fd){//重置事件
+//     epoll_event event;
+//     event.data.fd=fd;
+//     event.events=EPOLLIN|EPOLLET|EPOLLONESHOT;
+//     epoll_ctl(epollfd,EPOLL_CTL_MOD,fd,&event);
+// }

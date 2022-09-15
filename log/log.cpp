a2f07c166dd090d5a299a073500a4d20646c04db
dxlearn <dx_learn@163.com>
2022-09-15 22:53:04 +0800

    update log

--- log/log.cpp
+++ log/log.cpp
@@ -53,6 +53,7 @@ bool Log::init(const char *file_name,int log_buf_size,int split_lines,int max_qu
         strncpy(dir_name,file_name,p - file_name +1);
         snprintf(log_full_name,255,"%s%d_%02d_%02d_%s",dir_name,my_tm.tm_year +1990,my_tm.tm_mon+1,my_tm.tm_mday,log_name);
     }
+    m_today = my_tm.tm_mday;
     m_fp = fopen(log_full_name,"a");
     if(m_fp == NULL)
     {
@@ -118,11 +119,38 @@ void Log::write_log(int level,const char *format,...)
     string log_str;
     m_mutex.lock();
 
-
+    //写入的具体时间内容格式
+    int n = snprintf(m_buf,48,"%d-%02d-%02d %02d:%02d:%02d.%06ld %s ",
+                    my_tm.tm_year + 1900,my_tm.tm_mon +1,my_tm.tm_mday,
+                    my_tm.tm_hour,my_tm.tm_min,my_tm.tm_sec,now.tv_usec,s);
     
+    int m = vsnprintf(m_buf+n,m_log_buf_size-1,format,valst);
 
-     
+    m_buf[n + m] = '\n';
+    m_buf[n + m + 1] = '\0';
+    log_str = m_buf;
 
+    m_mutex.unlock();
 
+    if(m_is_async && !m_log_queue->full())
+    {
+        m_log_queue->push(log_str);
+    }
+
+    else
+    {
+        m_mutex.lock();
+        fputs(log_str.c_str(),m_fp);
+        m_mutex.unlock();
+    }
+
+    va_end(valst);
+}
+
+void Log::flush(void)
+{
+    m_mutex.lock();
+    fflush(m_fp);
+    m_mutex.unlock();
 }
 
